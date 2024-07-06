d98d9a6130a30166984f4376d7c6db5445acf241
oneofthezombies <hunhoekim@gmail.com>
2024-07-06 18:59:04 +0900

    build: change function name

--- dev/init.py
+++ dev/init.py
@@ -8,20 +8,20 @@ from git import Repo
 OnVisit = Callable[[Repo], None]
 
 
-def visit_submodule_owners(repo_path: Path, on_visit: OnVisit):
+def init_submodule_rec(repo_path: Path, on_visit: OnVisit):
     repo = git.Repo(repo_path)
     if len(repo.submodules) == 0:
         return
     on_visit(repo)
     for submodule in repo.submodules:
-        visit_submodule_owners(repo_path / submodule.path, on_visit)
+        init_submodule_rec(repo_path / submodule.path, on_visit)
 
 
-def init_submodule(cwd: str):
+def on_init_submodule(cwd: str):
     print(f"updating submodules on {cwd}...")
     subprocess.check_call(
         "git submodule update --init --recursive", shell=True, cwd=cwd
     )
 
 
-visit_submodule_owners(Path(), lambda repo: init_submodule(repo.working_dir))
+init_submodule_rec(Path(), lambda repo: on_init_submodule(repo.working_dir))
